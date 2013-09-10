ceea179bc13041ae06745dc7ff1c264fc0163b52
Sergio Torassa <sertorassa@hotmail.com>
2013-09-10 18:05:12 +0200

    Weights now are moving

--- NeuralNet.java
+++ NeuralNet.java
@@ -8,6 +8,7 @@ public class NeuralNet {
    private Neuron[][] net;
    private int size;
    private int hiddenLayers;
+   double[][] delta;
    private double min = -1, max = +1;
 
    /**
@@ -24,6 +25,7 @@ public class NeuralNet {
 
       size = _size;
       hiddenLayers = _hiddenLayers;
+      delta = new double[hiddenLayers + 2][];
       Synapse syn;
 
       // create the L layer of the net
@@ -92,6 +94,7 @@ public class NeuralNet {
                "Input size does not match network topology");
 
       // set the output of the L layer neurons
+      net[hiddenLayers + 1][0].output = 1;
       for (int i = 1; i < size; i++)
          net[hiddenLayers + 1][i].output = input[i - 1];
    }
@@ -104,12 +107,16 @@ public class NeuralNet {
       double signal = 0;
 
       // compute the network excluding the layer 0
-      for (int i = hiddenLayers; i > 0; i--)
+      for (int i = hiddenLayers; i > 0; i--) {
+         net[i][0].output = 1;
          for (int t = 1; t < net[i].length; t++) {
+            signal = 0;
             for (int k = 0; k < net[i + 1].length; k++)
-               signal += net[i + 1][k].output * net[i][t].prev.get(k).weight;
+               signal += net[i + 1][k].output
+                     * net[i][t].prev.get(t - 1).weight;
             net[i][t].output = Math.tanh(signal);
          }
+      }
 
       // compute the layer 0
       signal = 0;
@@ -127,27 +134,59 @@ public class NeuralNet {
     *           the constant for the update of weights
     */
    public void update(double target, double eta) {
-      double[][] delta = new double[hiddenLayers + 1][];
       for (int i = 0; i < hiddenLayers + 2; i++)
          delta[i] = new double[net[i].length];
-      
-      double sum = 0;
 
       // compute the derivative of squared error (delta of last node)
       delta[0][0] = 2 * (net[0][0].output - target)
             * (1 - Math.pow(Math.tanh(net[0][0].signal), 2));
-
-      // compute the first hidden layer deltas
-      for (int i = 0; i < net[1].length; i++)
-         delta[1][i] = (1 - Math.pow(net[1][i].output, 2))
-               * net[1][i].next.get(0).weight * delta[0][0];
+       System.out.println(net[0][0].output - target);
 
       // compute all the other layer deltas
+      for (int i = 1; i < hiddenLayers + 1; i++)
+         for (int t = 0; t < net[i].length; t++)
+            delta[i][t] = evaluateDelta(i, t);
+
+      // // compute the first hidden layer deltas
+      // for (int i = 0; i < net[1].length; i++)
+      // delta[0][0] = evaluateDelta(net[1][i].output,
+      // net[1][i].next.get(0).weight, delta[0][0], 1);
+      //
+      // // compute all the other layer deltas
+      // for (int i = 1; i < hiddenLayers + 2; i++)
+      // for (int t = 1; t < net[i].length; t++)
+      // for (int k = 0; k < net[i][t].next.size(); k++)
+      // sum += 1;
+
+      // compute the weights
       for (int i = 1; i < hiddenLayers + 2; i++)
-         for (int t = 1; t < net[i].length; t++)
-            for (int k = 0; k < net[i][t].next.size(); k++)
-               sum += 1;
-      //TODO
+         for (int t = 0; t < net[i].length; t++) {
+            if (i == 1) {
+               double var = eta * net[i][t].output * delta[i][t];
+               net[i][t].next.get(0).weight -= eta * net[i][t].output
+                     * delta[i][t];
+            } else
+               for (int k = 1; k < net[i - 1].length; k++) {
+                  double var = eta * net[i][t].output * delta[i][t];
+                  net[i][t].next.get(k - 1).weight -= eta * net[i][t].output
+                        * delta[i][t];
+               }
+
+         }
+
+      double sum;
+      for (int i = 0; i < hiddenLayers + 2; i++) {
+         sum = 0;
+         for (int t = 0; t < net[i].length; t++) {
+            for (Synapse syn : net[i][t].next)
+               sum += syn.weight;
+         }
+//         System.out.println("Sum of layer " + i + " weights: " + sum);
+      }
+
+      // update all the output and signals
+      compute();
+
    }
 
    /**
@@ -190,14 +229,31 @@ public class NeuralNet {
     *           arguments
     */
    public static void main(String[] args) {
-      int[] layerSize = { 3, 3 };
-      double[] input = { 1d, 2d, 3d, 4d, 5d };
-      NeuralNet net = new NeuralNet(5, 2, layerSize);
+      int[] layerSize = { 10, 10 };
+      double[] input = new double[10];
+      Random r = new Random();
+      int sum = 0;
+
+      NeuralNet net = new NeuralNet(10, 2, layerSize);
+
+      for (int t = 0; t < TRAINING_SIZE; t++) {
+         for (int i = 0; i < 10; i++) {
+            input[i] = r.nextInt(2);
+            sum += input[i];
+         }
+         net.setInput(input);
+         net.compute();
+         net.update(sum % 2, 0.9);
+         sum = 0;
+      }
+
+      for (int i = 0; i < 10; i++) {
+         input[i] = r.nextInt(2);
+         sum += input[i];
+      }
       net.setInput(input);
       net.compute();
-      System.out.println(net.getRealOutput());
-      net.scale(-5, 5);
-      System.out.println(net.getScaledOutput());
+      System.out.println(sum % 2 + " -> " + net.getRealOutput());
    }
 
    private class Neuron {
@@ -263,4 +319,26 @@ public class NeuralNet {
       }
    }
 
+   private double evaluateDelta(int currentLayer, int currentIndex) {
+      double result = 0;
+
+      // if layer 1 is updating, the following layer (the 0) does not have
+      // the bias neuron
+      if (currentLayer == 1)
+         result = net[currentLayer][currentIndex].next.get(0).weight
+               * delta[0][0];
+      else
+         for (int i = 0; i < net[currentLayer - 1].length - 1; i++) {
+            double _weight = net[currentLayer][currentIndex].next.get(i).weight;
+            double _delta = delta[currentLayer + 1][i];
+            result += net[currentLayer][currentIndex].next.get(i).weight
+                  * delta[currentLayer - 1][i];
+         }
+
+      result *= 1 - Math.pow(net[currentLayer][currentIndex].output, 2d);
+
+      return result;
+   }
+
+   private final static int TRAINING_SIZE = 100000;
 }
